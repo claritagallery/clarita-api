08464426008ccf1db32851e44cfa8112ff1a2a5e
Fidel Ramos <f@fidelramos.net>
2023-04-20 08:50:36 +0100

    Emacs projectile setup in dir-locals.el
    https://docs.projectile.mx/projectile/projects.html#storing-project-settings

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . (
+         (projectile-project-package-cmd . "docker build -t clarita-api:latest .")
+         (projectile-project-run-cmd . "pipenv run start")
+         (projectile-project-test-cmd . "pipenv run test"))))
