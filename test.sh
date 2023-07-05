24d959c7ff669de41a93bde5b8a46d768fa313db
Fidel Ramos <f@fidelramos.net>
2023-07-05 23:04:29 +0100

    Set ROOT_MAP in tests
    Needed for test_photos_file tests to find the real image file, otherwise
    the Digikam DB uses the absolute path from my laptop (/home/fidel/...)

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 export DATABASE_MAIN_PATH=tests/digikam_db/digikam4.db
 export DATABASE_THUMBNAIL_PATH=tests/digikam_db/thumbnails-digikam.db
 export CORS_ORIGINS='["*"]'
+export ROOT_MAP="{\"1\": \"$(pwd)/tests/images/\"}"
 pytest $*
