5f108f7c6529b752540b67bbfcd6ab2fe0380cf6
Fidel Ramos <contact.gyldd@8shield.net>
2024-09-06 11:04:31 +0100

    pylsp autoimport config in dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,20 @@
 ((nil . ((projectile-project-package-cmd . "docker build -t clarita-api:latest .")
          (projectile-project-run-cmd . "rye run dev")
          (projectile-project-test-cmd . "rye test")))
+ (python-ts-mode . ((eglot-workspace-configuration . (:pylsp
+                                                      (:plugins
+                                                       ;; Automatically find and add missing imports
+                                                       ;; https://github.com/python-lsp/python-lsp-server/blob/develop/docs/autoimport.md
+                                                       ;; https://gist.github.com/doolio/8c1768ebf33c483e6d26e5205896217f
+                                                       (:rope_autoimport
+                                                        (:code_actions (:enabled t)
+                                                         :completions (:enabled t)
+                                                         :enabled t
+                                                         ;; enabling in-memory DB causes python-lsp-server to hang and become unresponsive for some reason
+                                                         ;; :memory t
+                                                         ))
+                                                       :rope_completion
+                                                       (:eager :json-false
+                                                               :enabled t)
+                                                       )))))
  )
